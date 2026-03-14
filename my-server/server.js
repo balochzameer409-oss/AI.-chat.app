c417295c3d372519106d993af1c162ba342c81cd
zameer <balochzameer409@gmail.com>
2026-03-15 04:51:40 +0500

    Update server.js

--- my-server/server.js
+++ my-server/server.js
@@ -18,7 +18,7 @@ app.post('/ai', async (req, res) => {
         'X-Title': 'AI Chat'
       },
       body: JSON.stringify({
-        model: 'google/gemma-3-4b-it:free',
+    model: 'openrouter/auto',
         messages: [
           { role: 'system', content: system || 'اپ ایک مددگار اے ائی ہو' },
           { role: 'user', content: message }
